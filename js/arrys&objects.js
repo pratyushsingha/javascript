ea967d6086f548abc07f8566736d339c98703236
pratyushsingha <pratyushsingha83@gmail.com>
2023-02-22 13:47:45 +0530

    second commit

---
+++ js/arrys&objects.js
@@ -0,0 +1,19 @@
+// let employee ={
+//     name:"pratysh",
+//     salary : 10, 
+//     friend : "sharadiya",
+//     "friend name":"angshu"
+// }
+// console.log(employee)
+
+
+// let namess = new Array(11,2 , 8, "pratyush");
+let name = new Array(23,34);
+console.log(name.length);
+name = name.sort();
+name.push("this is pushed");
+console.log(name);
+
+let arr = new Array(10);
+console.log(arr);
+
