dd1e5e835be9f180a6fc19d3d5c09221db459ef1
pratyushsingha <pratyushsingha83@gmail.com>
2023-07-12 15:39:54 +0530

    practising and more practising

--- js/object.js
+++ js/object.js
@@ -1,37 +1,86 @@
-// // const a ={
-// //     name:'john',
-// //     age:30
+// // // const a ={
+// // //     name:'john',
+// // //     age:30
+// // // }
+// // // console.log(a)
+
+
+
+
+// // const b = {
+// //     name: 'pratyush',
+// //     printSurname: () => {
+// //         console.log("singh")
+// //     }
 // // }
-// // console.log(a)
 
+// // const a = "hellow world"
+// // b.added="value is added"
+// // // console.log(a)
+// // console.log(b)
+// // let a=23;
+// // let b=a;
+// // a=24
+// // console.log(a,b)
 
+// let a = {
+//     name: "pratyush",
+//     surName: "singha"
+// }
 
+// const b = a
+// console.log(a)
+// console.log(b)
 
-// const b = {
-//     name: 'pratyush',
-//     printSurname: () => {
-//         console.log("singh")
+// a.name = "changed"
+// console.log(a)
+// console.log(b)
+
+// const weight={
+//     weight:452,
+//     height:4554,
+//     quote:()=>{
+//         console.log("i am a programer")
+//     }
+// }
+// factory function
+// function wwweSuperStar(a,b,quote) {
+//     return{
+//         weight:a,
+//         height:b,
+//         quote:()=>{
+//             console.log(quote)
+//         }
 //     }
 // }
 
-// const a = "hellow world"
-// b.added="value is added"
-// // console.log(a)
-// console.log(b)
-// let a=23;
-// let b=a;
-// a=24
-// console.log(a,b)
+// const temp= wwweSuperStar(10,45, "sample sex")
+// console.log(temp)
 
-let a = {
-    name: "pratyush",
-    surName: "singha"
-}
 
-const b = a
-console.log(a)
-console.log(b)
+// constructor function
 
-a.name = "changed"
-console.log(a)
-console.log(b)
+// function Star(a,b) {
+//     this.weight=a
+//     this.height=b
+// }
+// const romanRance= new Star(185,457)
+// console.log(romanRance.constructor)
+
+// function Enemy(power,speed) {
+//     this.power=power
+//     this.speed=speed
+// }
+
+// const enemies=[]
+
+// for(let i=0;i<100;++i){
+//     enemies.push(new Enemy(Math.floor(Math.random()*100),Math.floor(Math.random()*100)))
+// }
+// console.log(enemies)
+
+// const arr=[12,215,1256,1235,45]
+
+// const newArr= new Array(...arr,457,45124)
+// console.log(arr)
+// console.log(newArr)
